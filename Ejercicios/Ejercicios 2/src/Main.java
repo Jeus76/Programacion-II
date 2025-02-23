c8467daf5ed5e56c15ef9088965ad678f00b7cdd
Jeus64 <jesushm06@gmail.com>
2025-02-23 10:21:52 +0100

    23/02/2025

--- Ejercicios/Ejercicios 2/src/Main.java
+++ Ejercicios/Ejercicios 2/src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
-    Mates mates = new Mates();
+
 
     public static void main(String[] args){
+        int n = 7;
+        int [] listado = {1,2,3,4,7};
+        double [] dListado = {1,2,3,4,7};
+        Mates mates = new Mates();
+        System.out.println("El sumatorio de " + n + " es: " + mates.sumatorioN(7));
+        System.out.println("El factorial de " + n + " es: " + mates.factorialN(7));
+        System.out.println("La potencia de 2 a la 6 es " + mates.potenciaN(2,6));
+        System.out.println("La suma de la lista de numeros [1, 2, 3, 4 ,7] es: " + mates.sumaLista(listado,0));
+        System.out.println("La media de la lista de numeros [1, 2, 3, 4, 7] es: " + mates.mediaListaRecursiva(listado,4));
+        System.out.println("La desviación típica de una lista de numeros [1,2,3,4,7] es: " + mates.desviacionListaRecursiva(dListado, 4));
+        System.out.println("La suma de pares hasta 7 es: " + mates.sumaPares(7));
+        System.out.println("La suma de pares de una lista de numeros [1,2,3,4,7] es: " + mates.sumaParesLista(listado,4));
+
 
     }
 }
--- Ejercicios/Ejercicios 2/src/Mates.java
+++ Ejercicios/Ejercicios 2/src/Mates.java
@@ -1,139 +1,95 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Mates {
-    private long n;
-    private Scanner sc;
-    private int carga;
-    private int natural;
-    List<Integer> numeros = new ArrayList<>();
 
-    public Mates() {
-        this.n = 0;
-        this.carga = 0;
-        this.natural = 0;
+
+    /**
+     * 1. La suma 0 + 1 + 2 + ... + n.
+     * 2. El factorial de un número.
+     * 3. La potencia n-ésima de un número natural.
+     * 4. La suma de los elementos de una lista de números.
+     */
+    public static int sumatorioN(int n) {
+
+        if (n <= 0) {
+            return 0;
+        } else {
+            return n + sumatorioN(n - 1);
+        }
+    }
+    public static int factorialN(int n) {
+        if (n <= 1) {
+            return 1;
+        }else {
+            return  n *= factorialN(n - 1);     }
+    }
+    public static int potenciaN(int natural, int n) {
+        if (n == 0){
+            return 1;
+        } else {
+            return natural * potenciaN(natural, n-1);
+        }
+    }
+    public static int sumaLista(int[] lista, int n) {
+        if ( n == lista.length) {
+            return 0;
+        }
+        return lista[n] + sumaLista(lista, n + 1);
+    }
+    public static double mediaLista(int [] comandos){
+        int elementos = comandos.length;
+        double suma = mediaListaRecursiva(comandos, elementos-1);
+        return suma/elementos;
+    }
+    public static double mediaListaRecursiva(int [] comandos, int elementos){
+        if (elementos >= 0) {
+            return comandos [elementos] + mediaListaRecursiva(comandos, elementos - 1);
+        }else{
+            return 0;
+        }
+    }
+    public static double desviacionLista(double [] comandos) {
+        int elementos = comandos.length;
+        double suma = desviacionListaRecursiva(comandos,elementos);
+        double media = suma/elementos;
+        double sumaDiferenciasCuadradas = desviacionListaRecursiva1(comandos,media, elementos);
+        return Math.sqrt(sumaDiferenciasCuadradas/elementos);
+    }
+    public static double desviacionListaRecursiva(double [] comandos,int elementos) {
+        if (elementos >= 0) {
+            return comandos [elementos] + desviacionListaRecursiva(comandos, elementos - 1);
+        } else {
+            return 0;
+        }
+    }
+    public static double desviacionListaRecursiva1(double [] comandos, double media, int elementos) {
+        if (elementos >= 0) {
+            return Math.pow(comandos[elementos]-media,2) + desviacionListaRecursiva1(comandos,media,elementos - 1);
+        } else {
+            return 0;
+        }
+    }
+    public static int sumaPares(int n){
+        if (n>0){
+            if (n%2==0){
+                return n + sumaPares(n-2);
+            } else {
+                return sumaPares(n-1);
+            }
+        } else {
+            return 0;
+        }
+    }
+    public static int sumaParesLista(int [] comandos, int n){
+        if (n>=0){
+            if (comandos[n]%2==0){
+                return comandos[n] + sumaParesLista(comandos, n-1);
+            } else {
+                return sumaParesLista(comandos, n-1);
+            }
+        } else {
+            return 0;
+        }
     }
 
 
-
-    public void sumatorioN() {
-    System.out.print("Introduzca el número final del sumatorio: ");
-    n = 6;
-    carga = 0;
-
-    for (int i = 1; i <= n; i++) {
-        carga += i;
-    }
-    System.out.println("Resultado: " + carga);
-}
-
-    public void factorialN() {
-        System.out.println("Introduzca el numero final del factorial: ");
-        n = sc.nextInt();
-        carga = 1;
-        for (int i = 1; i <= n; i++) {
-            carga *= i;
-
-        }
-        System.out.println("Resultado: " + carga);
-    }
-
-    public void potenciaN() {
-        System.out.println("Introduzca el numero natural que desea potenciar: ");
-        natural = sc.nextInt();
-        System.out.println("Introduzca la potencia: ");
-        n = sc.nextInt();
-        carga = 1;
-        for (int i = 0; i < n; i++) {
-            carga *= natural;
-        }
-        System.out.println("Resultado: " + carga);
-    }
-
-    public void sumaLista() {
-        System.out.println("Ingrese la cantidad de números en la lista:");
-        int cantidad = sc.nextInt();
-
-        System.out.println("Ingrese los números:");
-        for (int i = 0; i < cantidad; i++) {
-            numeros.add(sc.nextInt());
-        }
-
-        int suma = 0;
-        for (int num : numeros) {
-            suma += num;
-        }
-
-        System.out.println("Suma de los elementos de la lista: " + suma);
-
-    }
-
-    public void mediaLista() {
-        System.out.println("Ingrese la cantidad de números en la lista:");
-        int cantidad = sc.nextInt();
-
-        if (cantidad == 0) {
-            System.out.println("No se puede calcular la media de una lista vacía.");
-            return;
-        }
-
-        System.out.println("Ingrese los números:");
-        for (int i = 0; i < cantidad; i++) {
-            numeros.add(sc.nextInt());
-        }
-
-        int suma = 0;
-        for (int num : numeros) {
-            suma += num;
-
-        }
-
-        double media = (double) suma / cantidad;
-        System.out.println("Media aritmética de la lista: " + media);
-
-    }
-    public void desviacionTipica() {
-        List<Integer> numeros = new ArrayList<>();
-        System.out.println("Ingrese la cantidad de números en la lista:");
-        int cantidad = sc.nextInt();
-
-        if (cantidad == 0) {
-            System.out.println("No se puede calcular la desviación típica de una lista vacía.");
-            return;
-        }
-
-        System.out.println("Ingrese los números:");
-        for (int i = 0; i < cantidad; i++) {
-            numeros.add(sc.nextInt());
-        }
-
-        // Calcular la media
-        int suma = 0;
-        for (int num : numeros) {
-            suma += num;
-        }
-        double media = (double) suma / cantidad;
-
-        // Calcular la varianza
-        double sumaDifCuadradas = 0;
-        for (int num : numeros) {
-            sumaDifCuadradas += Math.pow(num - media, 2);
-        }
-        double varianza = sumaDifCuadradas / cantidad;
-
-        // Calcular la desviación típica
-        double desviacion = Math.sqrt(varianza);
-
-        System.out.println("Desviación típica de la lista: " + desviacion);
-    }
-    public static int sumaPares(int n) {
-        if (n < 2) return 0;
-        if (n % 2 != 0) return sumaPares(n - 1);
-        return n + sumaPares(n - 2);
-    }
-    public static int sumaParesLista(int[] lista, int i) {
-        if (i >= lista.length) return 0;
-        return (lista[i] % 2 == 0 ? lista[i] : 0) + sumaParesLista(lista, i + 1);
-    }
 }
